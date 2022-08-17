24595f1a84686051a22f4621005fc1e9610ca120
sathyamd005 <110916179+sathyamd005@users.noreply.github.com>
2022-08-18 01:27:46 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,57 @@
+ let myResume={
+    "personal Information":{
+        "Name": "sathya M ",
+         "Age":"21",
+         "Mobile No":9176504312,
+         "Email Id":"sathyamd0507@gamil.com",
+         "degree":"BE(cse)",
+         "location":{
+            "Address":"NO:4/244, venkatesapuram 1st street",
+            "place":"kottivakkam",
+             "postalCode": 600041,
+             "city": "Chennai",
+             "state": "Tamilnadu",
+             "country": "India"   
+        },
+    "profiles":[
+            {
+              "website":"www.linkedin.com/in/sathya-m-012205245",
+              "github":"https://github.com/sathyamd005"
+            }
+          ]
+},
+    "Acedemics":[
+            {
+              "institution":"Thangavelu Engineering College",
+              "department": "Computer Science And Engineering",
+              "year":"final year",
+            }
+          ],
+    "Skills": [
+            {
+              "name": "python,javascript",
+              "level": "beginer",
+              "project":[
+                "Developed android mobile application to perform arithmetic calculations"
+              ]
+            
+            }
+          ],
+    "Languages": [
+            {
+              "language": "Tamil,English",
+            }
+          ],
+    "Interests":[
+            {
+              "name": "script writter,Playing Volley ball",
+            }
+          ]
+        }
+        console.log(myResume);
+
+            
+         
+
+    
+
